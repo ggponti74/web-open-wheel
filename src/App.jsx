0f9db59ab4db86523fab99384a366cddd45bd4b2
ggponti74 <ggponti74@hotmail.com>
2026-08-21 15:13:01 +0000

    fix: update title in index.html and display version number in App component

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,7 @@ export function App() {
         onSelect={selectCategory}
       />
       <SeriesNav activeSeries={currentSeries} onSelect={setCurrentSeries} />
+    <p>__VERSION__NUMBER__</p>
     </div>
   );
 }
