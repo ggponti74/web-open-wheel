d04d92785341f8e3c3ebb2ea25e67123c231a0d1
ggponti74 <ggponti74@hotmail.com>
2026-08-22 11:29:28 -0400

    Add country flags utility function

---
+++ src/utils/countryFlags.js
@@ -0,0 +1,47 @@
+const COUNTRY_TO_ISO = {
+  'Australia': 'AU',
+  'China': 'CN',
+  'Japan': 'JP',
+  'Bahrain': 'BH',
+  'Saudi Arabia': 'SA',
+  'USA': 'US',
+  'Italy': 'IT',
+  'Monaco': 'MC',
+  'Spain': 'ES',
+  'Canada': 'CA',
+  'Austria': 'AT',
+  'UK': 'GB',
+  'Hungary': 'HU',
+  'Belgium': 'BE',
+  'Netherlands': 'NL',
+  'Azerbaijan': 'AZ',
+  'Singapore': 'SG',
+  'Mexico': 'MX',
+  'Brazil': 'BR',
+  'Qatar': 'QA',
+  'UAE': 'AE',
+  'Portugal': 'PT',
+  'France': 'FR',
+  'Germany': 'DE',
+  'Malaysia': 'MY',
+  'South Korea': 'KR',
+  'India': 'IN',
+  'Turkey': 'TR',
+  'Russia': 'RU',
+  'Vietnam': 'VN',
+  'South Africa': 'ZA',
+  'Argentina': 'AR',
+  'Sweden': 'SE',
+  'Switzerland': 'CH',
+  'Morocco': 'MA',
+};
+
+export function getFlagEmoji(countryName) {
+  const iso = COUNTRY_TO_ISO[countryName];
+  if (!iso) return '';
+  return iso
+    .toUpperCase()
+    .replace(/./g, (char) =>
+      String.fromCodePoint(char.charCodeAt(0) + 127397)
+    );
+}
