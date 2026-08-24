b47c15c04519f3273145f301e464abf1ff73ecd4
ggponti74 <ggponti74@hotmail.com>
2026-08-24 13:07:01 -0400

    Update NewsList.jsx

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -12,16 +12,3 @@ export function NewsList({ items }) {
   );
 }
 
-export function NewsList({ items }) {
-  return (
-    <ul class="news-list">
-      {items.map((item) => (
-        <li key={item.link} class="news-list__item">
-          <a href={item.link} target="_blank" rel="noopener noreferrer">
-            {item.title}
-          </a>
-        </li>
-      ))}
-    </ul>
-  );
-}
