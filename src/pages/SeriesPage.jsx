aa387f90204d3ecf994e52b992242c577a72d3c2
ggponti74 <ggponti74@hotmail.com>
2026-08-29 15:35:15 +0000

    Enhance F1 news update workflow to commit and push scraped data automatically

--- src/pages/SeriesPage.jsx
+++ src/pages/SeriesPage.jsx
@@ -1,42 +1,49 @@
-import { getSeries } from '../series.js';
-import { DriverStandings } from '../components/DriverStandings.jsx';
-import { TeamStandings } from '../components/TeamStandings.jsx';
-import { NextRace } from '../components/NextRace.jsx';
-import { FileBackedCategory } from '../components/FileBackedCategory.jsx';
+import { getSeries } from "../series.js";
+import { DriverStandings } from "../components/DriverStandings.jsx";
+import { TeamStandings } from "../components/TeamStandings.jsx";
+import { NextRace } from "../components/NextRace.jsx";
+import { FileBackedCategory } from "../components/FileBackedCategory.jsx";
 
 export function SeriesPage({ seriesId, categoryId }) {
   const series = getSeries(seriesId);
-  const categoryLabel = series.id === 'teams' ? series.teamsLabel : series.label;
+  const categoryLabel =
+    series.id === "teams" ? series.teamsLabel : series.label;
 
-  if (seriesId === 'f1') {
-    
-    if (categoryId === 'next-race') //return <NextRace />;
-      return (    
-      <div class="series-page">
-        <NextRace />
-      </div>
-    );
+  if (seriesId === "f1") {
+    if (categoryId === "next-race")
+      //return <NextRace />;
+      return (
+        <div class="series-page">
+          <NextRace />
+        </div>
+      );
 
-    if (categoryId === 'drivers') //return <DriverStandings />;
-    return (    
-      <div class="series-page">
-         <DriverStandings />
-      </div>
-    );
+    if (categoryId === "drivers")
+      //return <DriverStandings />;
+      return (
+        <div class="series-page">
+          <DriverStandings />
+        </div>
+      );
 
-    if (categoryId === 'teams') //return <TeamStandings />;
-    return (    
-      <div class="series-page">
-        <TeamStandings />
-      </div>
-    );
-    
+    if (categoryId === "teams")
+      //return <TeamStandings />;
+      return (
+        <div class="series-page">
+          <TeamStandings />
+        </div>
+      );
+
     // f1 news also goes through the file-backed path below
   }
 
-  return (    
+  return (
     <div class="series-page">
-      <FileBackedCategory seriesId={seriesId} categoryId={categoryId} />
+      <FileBackedCategory
+        key={`${seriesId}-${categoryId}`}
+        seriesId={seriesId}
+        categoryId={categoryId}
+      />
     </div>
   );
 }
