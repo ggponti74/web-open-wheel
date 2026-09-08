d3e69e975c159715d6f5965c4a1d489a2154855c
ggponti74 <ggponti74@hotmail.com>
2026-09-08 20:55:42 +0000

    Update version to 1.1.__BUILD_VERSION__.C and adjust body margin in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-const VERSION = "1.1.__BUILD_VERSION__.B";
+const VERSION = "1.1.__BUILD_VERSION__.C";
 const WHATS_NEW = "Added Formula 2, Formula 3, and Formula E driver standings.";
 
 import { useRef } from "preact/hooks";
