59a9443b306b88e7ab991d816f54449c9922b426
ggponti74 <ggponti74@hotmail.com>
2026-09-20 11:44:20 -0400

    Refactor NewsList component structure

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -2,26 +2,27 @@ import { LastUpdated } from "./LastUpdated.jsx";
 
 export function NewsList({ items, onSelect }) {
   return (
-    <ul class="news-list">
-      {items.map((item, index) => (
-        <li key={item.link} class="news-list__item">
-          {item.excerpt ? (
-            <button
-              type="button"
-              class="news-list__link"
-              onClick={() => onSelect(index)}
-            >
-              {item.title}
-            </button>
-          ) : (
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              {item.title}
-            </a>
-          )}
-        </li>
-      ))}
-    </ul>
-    <LastUpdated />
+    <>
+      <ul class="news-list">
+        {items.map((item, index) => (
+          <li key={item.link} class="news-list__item">
+            {item.excerpt ? (
+              <button
+                type="button"
+                class="news-list__link"
+                onClick={() => onSelect(index)}
+              >
+                {item.title}
+              </button>
+            ) : (
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                {item.title}
+              </a>
+            )}
+          </li>
+        ))}
+      </ul>
+      <LastUpdated />
     </>
   );
 }
