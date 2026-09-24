e58adb4d6244a546321df96dc6ec1001f2a17183
ggponti74 <ggponti74@hotmail.com>
2026-09-24 18:52:21 +0000

    Update scraped data [automated]

--- src/components/LastUpdated.jsx
+++ src/components/LastUpdated.jsx
@@ -1,4 +1,4 @@
-const __UPDATE_TIMESTAMP__ = "2026-09-24T18:44:57Z";
+const __UPDATE_TIMESTAMP__ = "2026-09-24T18:52:21Z";
 
 export function LastUpdated() {
   const date = new Date(__UPDATE_TIMESTAMP__);
