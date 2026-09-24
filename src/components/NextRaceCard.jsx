6a4cac2ba5281fceed3a9fd2ab0ed1c8ce55700f
ggponti74 <ggponti74@hotmail.com>
2026-09-24 16:44:32 -0400

    Refactor NextRaceCard to use new race properties

--- src/components/NextRaceCard.jsx
+++ src/components/NextRaceCard.jsx
@@ -21,14 +21,10 @@ export function NextRaceCard({ race }) {
         {race.name}
       </h2>
       {race.circuit && <p>{race.circuit}</p>}
-      <p>
-        {race.Circuit.Location.locality}, {race.Circuit.Location.country}
-      </p>
-      <p>{dateTime.toLocaleString()}</p>
-      <VenueMap
-        city={race.Circuit.Location.locality}
-        country={race.Circuit.Location.country}
-      />
+      {race.location && <p>{race.location}</p>}
+      {race.city && country}
+      {race.dateTime && <p>{new Date(race.dateTime).toLocaleString()}</p>}
+      <VenueMap city={race.city} country={country} />
     </div>
   );
 }
